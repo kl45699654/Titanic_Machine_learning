39e6b836f8bdf496058057322323148ef363157a
kl45699654 <410574577@qq.com>
2018-07-13 16:39:11 +0800

    泰坦尼克幸存者数据分析

---
+++ Titanic_farec.py
@@ -0,0 +1,129 @@
+# Load in our libraries
+import pandas as pd
+import numpy as np
+import re
+import sklearn
+import xgboost as xgb
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.tools as tls
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Going to use these 5 base models for the stacking
+from sklearn.ensemble import (RandomForestClassifier, AdaBoostClassifier,
+                              GradientBoostingClassifier, ExtraTreesClassifier)
+from sklearn.svm import SVC
+from sklearn.cross_validation import KFold
+
+# Load in the train and test datasets
+train = pd.read_csv('Titanic_data/train.csv')
+test = pd.read_csv('Titanic_data/test.csv')
+
+# Store our passenger ID for easy access
+PassengerId = test['PassengerId']
+
+full_data = [train, test]
+
+# Some features of my own that I have added in
+# Gives the length of the name
+train['Name_length'] = train['Name'].apply(len)
+test['Name_length'] = test['Name'].apply(len)
+# Feature that tells whether a passenger had a cabin on the Titanic
+train['Has_Cabin'] = train["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
+test['Has_Cabin'] = test["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
+
+# Feature engineering steps taken from Sina
+# Create new feature FamilySize as a combination of SibSp and Parch
+for dataset in full_data:
+    dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+# Create new feature IsAlone from FamilySize
+for dataset in full_data:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+# Remove all NULLS in the Embarked column
+for dataset in full_data:
+    dataset['Embarked'] = dataset['Embarked'].fillna('S')
+# Remove all NULLS in the Fare column and create a new feature CategoricalFare
+for dataset in full_data:
+    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
+train['CategoricalFare'] = pd.qcut(train['Fare'], 4)
+# Create a New feature CategoricalAge
+for dataset in full_data:
+    age_avg = dataset['Age'].mean()
+    age_std = dataset['Age'].std()
+    age_null_count = dataset['Age'].isnull().sum()
+    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
+    dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list
+    dataset['Age'] = dataset['Age'].astype(int)
+train['CategoricalAge'] = pd.cut(train['Age'], 5)
+
+
+# Define function to extract titles from passenger names
+def get_title(name):
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    # If the title exists, extract and return it.
+    if title_search:
+        return title_search.group(1)
+    return ""
+
+
+# Create a new feature Title, containing the titles of passenger names
+for dataset in full_data:
+    dataset['Title'] = dataset['Name'].apply(get_title)
+# Group all non-common titles into one single grouping "Rare"
+for dataset in full_data:
+    dataset['Title'] = dataset['Title'].replace(
+        ['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+for dataset in full_data:
+    # Mapping Sex
+    dataset['Sex'] = dataset['Sex'].map({'female': 0, 'male': 1}).astype(int)
+
+    # Mapping titles
+    title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+    # Mapping Embarked
+    dataset['Embarked'] = dataset['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
+
+    # Mapping Fare
+    dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
+    dataset.loc[dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+
+    # Mapping Age
+    dataset.loc[dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[dataset['Age'] > 64, 'Age'] = 4;
+
+drop_elements = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp']
+train = train.drop(drop_elements, axis = 1)
+train = train.drop(['CategoricalAge', 'CategoricalFare'], axis = 1)
+test  = test.drop(drop_elements, axis = 1)
+
+colormap = plt.cm.RdBu
+plt.figure(figsize=(14,12))
+plt.title('Pearson Correlation of Features', y=1.05, size=15)
+sns.heatmap(train.astype(float).corr(),linewidths=0.1,vmax=1.0,
+            square=True, cmap=colormap, linecolor='white', annot=True)
+g = sns.pairplot(train[[u'Survived', u'Pclass', u'Sex', u'Age', u'Parch', u'Fare', u'Embarked',
+       u'FamilySize', u'Title']], hue='Survived', palette = 'seismic',size=1.2,diag_kind = 'kde',diag_kws=dict(shade=True),plot_kws=dict(s=10) )
+g.set(xticklabels=[])
+
+plt.show()
